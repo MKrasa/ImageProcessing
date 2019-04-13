f1b10c7cf047e5037b7c1960a58ce33763a3a57e
MKrasa <30436081+MKrasa@users.noreply.github.com>
2019-04-13 15:37:19 +0200

    Add ForkJoin image processing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
+    public static List<String> GetDirs(String path) {
+
+        List<String> dirs = new ArrayList<>();
+        try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+            dirs = walk.map(x -> x.toString())
+                    .filter(f -> f.endsWith(".jpg")).collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  dirs;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String path = "D:\\Photos_Java";
+        Integer count = 4;
+
+        List<String> lDirectories = GetDirs(path);
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        try {
+            forkJoinPool.invoke(new ForkJoinManager(lDirectories, count));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
